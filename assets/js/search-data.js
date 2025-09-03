01162ffecc4117511326e3a76fec36391c25a455
lakshyakh93 <lakshaya.khandelwal@gmail.com>
2025-09-03 14:42:44 +0000

    Deploying to gh-pages from @ lakshyakh93/lakshyakh93.github.io@6a21edcddf57b2377d255c44b9d035502f56bd01 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/resources/";
           },
-        },{id: "post-the-mighty-xgboost-2-0-is-here",
+        },{id: "post-conversational-bot-for-enterprise-apis-using-llm-agents-vectordb-and-gpt4",
+      
+        title: 'Conversational Bot for Enterprise APIs using LLM Agents, VectorDB and GPT4 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "A comprehensive guide on building conversational bots for enterprise APIs using LLM agents, vector databases, and GPT-4",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://medium.com/walmartglobaltech/conversational-bot-for-enterprise-apis-using-llm-agents-vectordb-and-gpt4-168bfcb2be0d", "_blank");
+        
+      },
+    },{id: "post-the-mighty-xgboost-2-0-is-here",
       
         title: "The Mighty XgBoost 2.0 is here",
       
